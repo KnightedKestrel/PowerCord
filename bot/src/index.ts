06ef2e50f963f9c2422b7529ad59355bc02113fa
KnightedKestrel <25458428+KnightedKestrel@users.noreply.github.com>
2024-11-14 18:24:16 -0600

    Initialize Discord bot

---
+++ bot/src/index.ts
@@ -0,0 +1,16 @@
+require('dotenv').config()
+import { Client, Events, GatewayIntentBits } from "discord.js";
+
+const token = process.env.DISCORD_TOKEN;
+
+console.log("Bot is starting...");
+
+const client = new Client({
+    intents: [GatewayIntentBits.Guilds]
+});
+
+client.once(Events.ClientReady, readyClient => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+client.login(token);
