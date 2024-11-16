951bc27c4f34f7f0ed2c22aee5907fa7a05b53d7
KnightedKestrel <25458428+KnightedKestrel@users.noreply.github.com>
2024-11-15 19:57:08 -0600

    Lint and formatting commands

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,44 +1,50 @@
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import globals from "globals";
-import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import globals from 'globals';
+import tsParser from '@typescript-eslint/parser';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const filename = fileURLToPath(import.meta.url);
 const dirname = path.dirname(filename);
 const compat = new FlatCompat({
     baseDirectory: dirname,
     recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+    allConfig: js.configs.all,
 });
 
-export default [...compat.extends("eslint:recommended", "prettier"), {
-    plugins: {
-        "@typescript-eslint": typescriptEslint,
-    },
-
-    languageOptions: {
-        globals: {
-            ...globals.browser,
-            ...globals.node,
+export default [
+    ...compat.extends('eslint:recommended', 'prettier'),
+    {
+        plugins: {
+            '@typescript-eslint': typescriptEslint,
         },
 
-        parser: tsParser,
-        ecmaVersion: 2021,
-        sourceType: "commonjs",
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+            },
+
+            parser: tsParser,
+            ecmaVersion: 2021,
+            sourceType: 'commonjs',
+        },
+
+        rules: {
+            'no-console': 'warn',
+            'no-empty-function': 'error',
+            'no-return-assign': 'error',
+
+            'no-shadow': [
+                'error',
+                {
+                    allow: ['err', 'resolve', 'reject'],
+                },
+            ],
+
+            'prefer-const': 'warn',
+        },
     },
-
-    rules: {
-        "no-console": "warn",
-        "no-empty-function": "error",
-        "no-return-assign": "error",
-
-        "no-shadow": ["error", {
-            allow: ["err", "resolve", "reject"],
-        }],
-
-        "prefer-const": "warn",
-    },
-}];
+];
