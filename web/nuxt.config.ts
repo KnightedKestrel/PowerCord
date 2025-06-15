92e0eb02f306b5fdff9ec942d6634741bbde8bbf
KnightedKestrel <25458428+KnightedKestrel@users.noreply.github.com>
2025-06-14 21:21:55 -0500

    Clean up nuxt.config

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -3,14 +3,6 @@ export default defineNuxtConfig({
     compatibilityDate: '2025-05-15',
     devtools: { enabled: true },
 
-    css: ['~/assets/scss/main.scss'],
-    postcss: {
-        plugins: {
-            '@tailwindcss/postcss': {},
-            autoprefixer: {},
-        },
-    },
-
     modules: [
         '@nuxt/icon',
         '@nuxt/image',
