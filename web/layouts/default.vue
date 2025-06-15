6ff0f965516bf58e70b1380d841c6a9f5099c7c1
KnightedKestrel <25458428+KnightedKestrel@users.noreply.github.com>
2025-06-14 21:21:07 -0500

    Clean up file

--- web/layouts/default.vue
+++ web/layouts/default.vue
@@ -6,12 +6,6 @@
     </div>
 </template>
 
-<!-- <style lang="postcss" scoped>
-body {
-    @apply bg-gray-800 font-sans text-gray-200;
-}
-</style> -->
-
 <style lang="scss">
 body {
     background-color: var(--color-gray-900);
