8c7a500786488b757bf743053204cbb3c2ec077e
G-Nith <nithhg130@gmail.com>
2025-06-15 18:13:03 -0300

    Checks date of CSV before downloading.
    Gets date from page and compares it to the db's. If unable to validate, download.
    Added Cheerio as Dependency.

--- bot/src/data/csvDownloader.ts
+++ bot/src/data/csvDownloader.ts
@@ -4,11 +4,37 @@ import path from 'path';
 import axios from 'axios';
 import unzipper from 'unzipper';
 import DatabaseManager from './database';
+import * as cheerio from 'cheerio';
 
 const ZIP_URL =
     'https://openpowerlifting.gitlab.io/opl-csv/files/openpowerlifting-latest.zip';
 const DOWNLOAD_DIR = path.resolve(__dirname, './downloads');
 const ZIP_PATH = path.join(DOWNLOAD_DIR, 'openpowerlifting-latest.zip');
+
+async function getLastUpdate(): Promise<string> {
+    try {
+        const url = 'https://openpowerlifting.gitlab.io/opl-csv/bulk-csv.html';
+        const response = await axios.get(url);
+        const cheer = cheerio.load(response.data);
+        const updatedText = cheer('body > div > div > div > div > main > ul > li')
+            .first()
+            .text()
+            .trim();
+        const noDotText = updatedText.replace('.', '');
+        const dateText = noDotText.replace('Updated:', '').trim();
+
+        if (!dateText) {
+            console.log(updatedText);
+            throw new Error('Could not find the date on the specified page.');
+        }
+
+        console.log(`Last CSV Update: ${dateText}`);
+        return dateText;
+    } catch (error) {
+        console.error('Failed to fetch updated date:', error);
+        throw error;
+    }
+}
 
 export async function csvDownloader(): Promise<{
     csvPath: string;
@@ -17,7 +43,24 @@ export async function csvDownloader(): Promise<{
     const db = DatabaseManager.getInstance().getDB();
 
     try {
-        // Ensure the downloads directory exists
+        // Compare dates
+        const getDate = await getLastUpdate();
+
+        const currentDate = db
+            .prepare('SELECT UpdatedDate FROM opl_data_version LIMIT 1')
+            .pluck()
+            .get();
+
+        if (currentDate === getDate) {
+            console.log('The CSV data is already up-to-date.');
+            await fsp.rm(DOWNLOAD_DIR, { recursive: true, force: true });
+            console.log('Temporary files cleaned up.');
+            return { csvPath: '', extractedDate: '' };
+        } else {
+            console.log('DB:', currentDate, 'CSV:', getDate)
+        }
+
+        // ZIP download
         await fsp.mkdir(DOWNLOAD_DIR, { recursive: true });
 
         console.log('Downloading the latest ZIP file...');
@@ -27,7 +70,6 @@ export async function csvDownloader(): Promise<{
             responseType: 'stream',
         });
 
-        // Save the downloaded ZIP file
         await new Promise<void>((resolve, reject) => {
             const writer = fs.createWriteStream(ZIP_PATH);
             response.data.pipe(writer);
@@ -43,6 +85,7 @@ export async function csvDownloader(): Promise<{
             });
         });
 
+        // Extract ZIP
         console.log('Extracting the ZIP file...');
         await new Promise<void>((resolve, reject) => {
             fs.createReadStream(ZIP_PATH)
@@ -66,9 +109,10 @@ export async function csvDownloader(): Promise<{
         }
 
         console.log(`Found dated folder: ${datedFolder}`);
-        const extractedDate = datedFolder.match(
-            /openpowerlifting-(\d{4}-\d{2}-\d{2})/,
-        )?.[1];
+        const extractedDate = getDate;
+        // const extractedDate = datedFolder.match(
+        //     /openpowerlifting-(\d{4}-\d{2}-\d{2})/,
+        // )?.[1];
 
         if (!extractedDate) {
             throw new Error(
@@ -76,23 +120,7 @@ export async function csvDownloader(): Promise<{
             );
         }
 
-        const currentDate = db
-            .prepare('SELECT UpdatedDate FROM opl_data_version LIMIT 1')
-            .pluck()
-            .get();
-
-        if (currentDate === extractedDate) {
-            console.log(
-                'The CSV data is already up-to-date. Deleting downloaded files.',
-            );
-
-            // Cleanup the extracted folder and the downloaded ZIP file
-            await fsp.rm(DOWNLOAD_DIR, { recursive: true, force: true });
-            console.log('Temporary files cleaned up.');
-            return { csvPath: '', extractedDate: '' };
-        }
-
-        // Determine the CSV file name dynamically
+        // Find CSV
         const extractedPath = path.join(DOWNLOAD_DIR, datedFolder);
         const extractedFiles = await fsp.readdir(extractedPath);
         const csvFileName = extractedFiles.find((file) =>
@@ -106,6 +134,7 @@ export async function csvDownloader(): Promise<{
         const csvPath = path.join(extractedPath, csvFileName);
         console.log(`CSV ready for processing: ${csvPath}`);
         return { csvPath, extractedDate };
+
     } catch (error) {
         console.error(
             'An error occurred during CSV download or processing:',
@@ -113,6 +142,7 @@ export async function csvDownloader(): Promise<{
         );
         throw error;
     } finally {
+        // Cleanup
         await fsp.access(ZIP_PATH);
         console.log('Deleting ZIP file...');
         await fsp.unlink(ZIP_PATH);
