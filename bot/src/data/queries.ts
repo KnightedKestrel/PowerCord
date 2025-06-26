26f3f0a9bde259227023bcd4b39f5ad3a27d5871
KnightedKestrel <25458428+KnightedKestrel@users.noreply.github.com>
2025-06-26 02:15:18 -0500

    Initial query refactor; Tests (#71)
    * Separate query file to eventually house all db queries
    * First working query test (HUGE)
    * Check for error logs in tests

---
+++ bot/src/data/queries.ts
@@ -0,0 +1,17 @@
+import logger from '../logger';
+import DatabaseManager from './database';
+
+const db = DatabaseManager.getInstance().getDB();
+
+export function getLatestUpdatedDate(): Promise<string | null> {
+    try {
+        const date = db
+            .prepare('SELECT UpdatedDate FROM opl_data_version LIMIT 1')
+            .pluck()
+            .get() as string | undefined;
+        return Promise.resolve(date ?? null);
+    } catch (error) {
+        logger.error('Failed to fetch UpdatedDate:', error);
+        return Promise.resolve(null);
+    }
+}
