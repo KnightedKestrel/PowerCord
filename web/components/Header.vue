69c774eaaf227c8f118ba2f5eeeb5670723a12e1
KnightedKestrel <25458428+KnightedKestrel@users.noreply.github.com>
2025-06-28 17:27:33 -0500

    Add mobile responsiveness

--- web/components/Header.vue
+++ web/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-    <header class="w-full border-b border-gray-800" role="banner">
+    <header class="relative z-50 w-full border-b border-gray-800" role="banner">
         <UContainer>
             <div class="flex h-16 items-center justify-between">
                 <ULink to="/">
@@ -14,37 +14,83 @@
                         >
                     </div>
                 </ULink>
-                <nav class="flex items-center gap-4" role="navigation">
-                    <ULink
-                        to="https://volta.net/KnightedKestrel/PowerCord"
-                        target="_blank"
-                        external
-                    >
-                        Roadmap
-                    </ULink>
-                    <ULink
-                        to="https://github.com/KnightedKestrel/PowerCord"
-                        target="_blank"
-                        external
-                    >
-                        Source
-                    </ULink>
+
+                <UNavigationMenu :items="navItems" class="hidden md:block" />
+
+                <USlideover
+                    :close="{
+                        color: 'neutral',
+                        variant: 'subtle',
+                        class: 'text-lg',
+                    }"
+                    class="md:hidden"
+                >
                     <UButton
-                        to="https://discord.gg/MZfchrRah4"
-                        target="_blank"
-                        size="lg"
-                        variant="outline"
-                        class="text-md"
-                        >Join Support Server</UButton
-                    >
-                </nav>
+                        :icon="isNavOpen ? 'i-lucide:x' : 'i-lucide:menu'"
+                        size="xl"
+                        color="neutral"
+                        variant="subtle"
+                        aria-label="Open navigation menu"
+                    />
+
+                    <template #body>
+                        <UNavigationMenu
+                            :items="navItems"
+                            orientation="vertical"
+                            class="-mx-2.5"
+                        />
+                    </template>
+                </USlideover>
             </div>
         </UContainer>
     </header>
 </template>
+
+<script setup lang="ts">
+import type { NavigationMenuItem } from '@nuxt/ui';
+import { ref } from 'vue';
+
+const isNavOpen = ref(false);
+const navItems = ref<NavigationMenuItem[][]>([
+    [
+        {
+            label: 'Roadmap',
+            icon: 'i-lucide-map',
+            to: 'https://volta.net/KnightedKestrel/PowerCord',
+            target: '_blank',
+        },
+        {
+            label: 'Source',
+            icon: 'i-simple-icons-github',
+            to: 'https://volta.net/KnightedKestrel/PowerCord',
+            target: '_blank',
+        },
+        {
+            label: 'Support Server',
+            icon: 'i-simple-icons-discord',
+            to: 'https://discord.gg/MZfchrRah4',
+            target: '_blank',
+        },
+    ],
+]);
+
+const openNav = () => {
+    isNavOpen.value = !isNavOpen.value;
+};
+
+watch(isNavOpen, (newValue) => {
+    if (newValue) {
+        document.body.classList.add('no-scroll');
+    } else {
+        document.body.classList.remove('no-scroll');
+    }
+});
+</script>
 
 <style lang="scss" scoped>
 .logo-text {
     font-family: 'Kanit';
 }
 </style>
+
+<!-- i-lucide:x -->
