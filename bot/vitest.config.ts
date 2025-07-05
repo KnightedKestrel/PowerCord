61870d306807230cf05146051441a1c65f278708
KnightedKestrel <25458428+KnightedKestrel@users.noreply.github.com>
2025-07-05 14:08:45 -0500

    Formatting

--- bot/vitest.config.ts
+++ bot/vitest.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  test: {
-    environment: 'node',
-    coverage: {
-      reporter: ['text', 'lcov'],
-      reportsDirectory: './coverage',
+    test: {
+        environment: 'node',
+        coverage: {
+            reporter: ['text', 'lcov'],
+            reportsDirectory: './coverage',
+        },
     },
-  },
 });
