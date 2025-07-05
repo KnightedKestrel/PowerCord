67aba0e4f1166c2332db550ddce68ad040cd8a5f
KnightedKestrel <25458428+KnightedKestrel@users.noreply.github.com>
2025-07-05 18:00:46 -0500

    Remove Nuxt config from coverage (#97)
    * Fix web coverage
    
    * Add Codecov badge

--- web/vitest.config.ts
+++ web/vitest.config.ts
@@ -11,6 +11,11 @@ export default defineVitestConfig({
         coverage: {
             reporter: ['text', 'lcov'],
             reportsDirectory: './coverage',
+            include: [
+                'components/**/*.{js,ts,vue}',
+                'layouts/**/*.{js,ts,vue}',
+                'pages/**/*.{js,ts,vue}',
+            ],
         },
     },
 });
