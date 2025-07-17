dad1528a6885921c5dc421da8b053a534332517c
KnightedKestrel <25458428+KnightedKestrel@users.noreply.github.com>
2025-07-17 08:01:36 -0500

    Upgraded to Nuxt v4.0

--- web/app/components/Footer.vue
+++ web/app/components/Footer.vue
@@ -7,7 +7,7 @@
                     <ULink
                         to="https://github.com/KnightedKestrel/PowerCord/blob/master/LICENSE"
                         target="_blank"
-                        class="text-white hover:text-primary"
+                        class="hover:text-primary text-white"
                         external
                         >AGPLv3</ULink
                     >
