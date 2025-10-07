e38add028b25fa84b9fa6165b13e5df1e858a55a
KnightedKestrel <25458428+KnightedKestrel@users.noreply.github.com>
2025-10-06 19:54:02 -0500

    Prettier formatting

--- bot/src/utils/health.ts
+++ bot/src/utils/health.ts
@@ -5,9 +5,9 @@ const app = express();
 const port = 3000;
 
 app.get('/health', (req, res) => {
-  res.send({ status: 'ok' });
+    res.send({ status: 'ok' });
 });
 
 app.listen(port, () => {
-  logger.info(`Health check is running on port ${port}`);
+    logger.info(`Health check is running on port ${port}`);
 });
