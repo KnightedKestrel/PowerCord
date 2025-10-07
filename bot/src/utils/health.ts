3c387ff511fb331f3c3641e7b8be218ca3fc22b9
KnightedKestrel <25458428+KnightedKestrel@users.noreply.github.com>
2025-10-06 19:56:04 -0500

    Initial Docker setup; Health check (#108)
    * Initial Docker setup; Health check
    
    * Prettier formatting

---
+++ bot/src/utils/health.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import logger from './logger';
+
+const app = express();
+const port = 3000;
+
+app.get('/health', (req, res) => {
+    res.send({ status: 'ok' });
+});
+
+app.listen(port, () => {
+    logger.info(`Health check is running on port ${port}`);
+});
