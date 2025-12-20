5e258f228112ec34b8530260af228e8118b798e1
KnightedKestrel <25458428+KnightedKestrel@users.noreply.github.com>
2025-12-20 11:18:30 -0600

    Add SSM access

--- terraform/main.tf
+++ terraform/main.tf
@@ -120,6 +120,11 @@ resource "aws_iam_role_policy_attachment" "ecr_read" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
+resource "aws_iam_role_policy_attachment" "ssm" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
 resource "aws_iam_instance_profile" "ec2_profile" {
   name = "powercord-bot-ec2-profile"
   role = aws_iam_role.ec2_role.name
