ab9f202d3acdd3f7efa3792546bc0ae22c8e8c82
kystrel <25458428+kystrel@users.noreply.github.com>
2026-03-31 23:33:20 -0500

    test(web): explicitly set v8 as coverage provider

--- web/vitest.config.ts
+++ web/vitest.config.ts
@@ -9,6 +9,7 @@ export default defineVitestConfig({
             },
         },
         coverage: {
+            provider: 'v8',
             reporter: ['text', 'lcov'],
             reportsDirectory: './coverage',
             include: [
